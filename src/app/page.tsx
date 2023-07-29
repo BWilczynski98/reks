9733474749b3a2f016ec2a57cf875f23fb6b2351
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-07-29 16:13:57 +0200

    Fix problem with: Attempted import error: 'useGetUsersQuery' is not exported from '@/redux/services/userApi' (imported as 'useGetUsersQuery').

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 import { decrement, increment } from "@/redux/features/counterSlice"
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
-import { useGetUsersQuery } from "@/redux/services/userApi"
 
 export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value)
   const dispatch = useAppDispatch()
-  const { data } = useGetUsersQuery(null)
 
   return (
     <main>
