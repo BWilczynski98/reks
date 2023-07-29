1b97849c14547b3cef7cba986a9ac4c5decc9b83
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-07-29 10:48:15 +0200

    Prettier and eslint startup

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,7 +2,6 @@ import type { NextAuthOptions } from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-
 export const options: NextAuthOptions = {
   providers: [
     CredentialsProvider({
@@ -12,7 +11,7 @@ export const options: NextAuthOptions = {
         password: { label: "password", type: "password", placeholder: "password" },
       },
       async authorize(credentials) {
-        const user = {id: "1", email: 'test@test.com', password: '1234'}
+        const user = { id: "1", email: "test@test.com", password: "1234" }
 
         return user
       },
