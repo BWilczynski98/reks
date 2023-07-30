db03bdaf2393018a0c0b818e9ee04a53d7210774
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-07-30 14:29:30 +0200

    Setting fonts

---
+++ src/app/lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Lato, Montserrat } from "next/font/google"
+
+export const headline = Lato({
+  weight: ["400", "700", "900"],
+  subsets: ["latin"],
+})
+
+export const body = Montserrat({
+  weight: ["200", "400", "500", "600", "700"],
+  subsets: ["latin"],
+})
