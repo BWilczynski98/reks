e86f0b6547e383ff1e13adf461d11a5e22d9dc2b
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-07-30 14:30:35 +0200

    Register page, setting validation and function handler for create new user in db

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+import React from "react"
+
+export const metadata: Metadata = {
+  title: "Reks - rejestracja",
+}
+
+const CartLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>
+}
+
+export default CartLayout
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,54 @@
 "use client"
 import { useCreateUserMutation } from "@/redux/services/userApi"
 import { useState } from "react"
+import { Banner } from "../assets/svg/banner"
+import { TextField } from "../components/UI/TextField"
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"
+import { Button } from "../components/UI/Button"
+import { useForm, Controller, SubmitHandler } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
-type UserNewAccountParams = {
-  email: string
-  password: string
-}
+const schema = yup
+  .object({
+    email: yup.string().email("Podany adres email jest nie poprawny").required("To pole nie może być puste"),
+    password: yup.string().required("To pole nie może być puste"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), undefined], "Hasła muszą być takie same") 
+      .required("To pole nie może być puste"),
+  })
+  .required()
+type FormData = yup.InferType<typeof schema>
 
 export default function RegisterPage() {
-  const [createUser] = useCreateUserMutation()
-  const [userNewAccountData, setUserNewAccountData] = useState<UserNewAccountParams>({
-    email: "",
-    password: "",
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   })
 
-  const registerNewUser = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    createUser(userNewAccountData)
+  const [passwordVisibility, setPasswordVisibility] = useState<boolean>(false)
+  const [confirmPasswordVisibility, setConfirmPasswordVisibility] = useState<boolean>(false)
+  const [createUser] = useCreateUserMutation()
+
+  const handleTogglePasswordVisbility = () => {
+    setPasswordVisibility((prev) => !prev)
+  }
+
+  const handleToggleConfirmPasswordVisbility = () => {
+    setConfirmPasswordVisibility((prev) => !prev)
+  }
+
+  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
+    createUser({ email: data.email, password: data.password })
       .unwrap()
       .then((res) => {
         console.log(res)
@@ -27,39 +59,107 @@ export default function RegisterPage() {
   }
 
   return (
-    <main>
-      <form onSubmit={registerNewUser}>
-        <div className="flex flex-col w-60">
-          <label htmlFor="email">Adres email</label>
-          <input
+    <main className="flex">
+      <div className="w-1/2 h-screen border">
+        <Banner />
+      </div>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-5 p-2"
+      >
+        <Controller
+          name="email"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label="Adres email"
+              type="email"
+              name="email"
+              id="email"
+              onChange={onChange}
+              value={value}
+              error={!!errors.email}
+              errorMessage={errors.email?.message}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label="Hasło"
+              type={passwordVisibility ? "text" : "password"}
+              name="password"
+              id="password"
+              onChange={onChange}
+              value={value}
+              icon={passwordVisibility ? <AiFillEye /> : <AiFillEyeInvisible />}
+              handleTogglePasswordVisbility={handleTogglePasswordVisbility}
+              error={!!errors.password}
+              errorMessage={errors.password?.message}
+            />
+          )}
+        />
+        <Controller
+          name="confirmPassword"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <TextField
+              label="Powtórz hasło"
+              type={confirmPasswordVisibility ? "text" : "password"}
+              name="confirmPassword"
+              id="confirmPassword"
+              onChange={onChange}
+              value={value}
+              icon={confirmPasswordVisibility ? <AiFillEye /> : <AiFillEyeInvisible />}
+              handleTogglePasswordVisbility={handleToggleConfirmPasswordVisbility}
+              error={!!errors.confirmPassword}
+              errorMessage={errors.confirmPassword?.message}
+            />
+          )}
+        />
+
+        {/* <div>
+          <TextField
             placeholder="email@email.com"
-            className="p-2 border border-gray-700"
-            name="email"
+            label="Adres email"
             type="email"
+            name="email"
             id="email"
-            required
-            onChange={(e) => setUserNewAccountData(() => ({ ...userNewAccountData, email: e.target.value }))}
-          />
-        </div>
-        <div className="flex flex-col w-60">
-          <label htmlFor="password">Hasło</label>
-          <input
-            placeholder="**********"
-            className="p-2 border border-gray-700"
-            name="password"
-            type="password"
-            id="password"
-            required
-            onChange={(e) => setUserNewAccountData(() => ({ ...userNewAccountData, password: e.target.value }))}
+            onChange={handleUserEmail}
+            value={userNewAccountData.email}
           />
         </div>
         <div>
-          <button
-            type="submit"
-            className="p-4 border border-gray-700"
-          >
-            zarejestruj
-          </button>
+          <TextField
+            placeholder="hasło"
+            label="Hasło"
+            type={passwordVisibility ? "text" : "password"}
+            name="password"
+            id="password"
+            onChange={handleUserPassword}
+            value={userNewAccountData.password}
+            icon={passwordVisibility ? <AiFillEye /> : <AiFillEyeInvisible />}
+            handleTogglePasswordVisbility={handleTogglePasswordVisbility}
+          />
+        </div>
+        <div>
+          <TextField
+            placeholder="hasło"
+            label="Powtórz hasło"
+            type={confirmPasswordVisibility ? "text" : "password"}
+            name="confirmPassword"
+            id="confirmPassword"
+            onChange={handleConfirmUserPassword}
+            value={userNewAccountData.confirmPassword}
+            icon={confirmPasswordVisibility ? <AiFillEye /> : <AiFillEyeInvisible />}
+            handleTogglePasswordVisbility={handleToggleConfirmPasswordVisbility}
+          />
+        </div> */}
+        <div>
+          <Button type="submit">Zarejestruj konto</Button>
         </div>
       </form>
     </main>
