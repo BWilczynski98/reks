21f0c6db6d06640cad6737b97383c3e104f6f982
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-08-02 11:57:33 +0200

    added loading status for button.

--- src/app/components/UI/Button/Button.tsx
+++ src/app/components/UI/Button/Button.tsx
@@ -1,21 +1,26 @@
+import { body } from "@/app/lib/fonts"
 import React from "react"
+import { LoadingIndicator } from "../LoadingIndicator"
 
 type Props = {
   children: React.ReactNode
   onClick?: () => void
   disabled?: boolean
   type?: "button" | "reset" | "submit"
+  loading?: boolean
 }
 
-export const Button = ({ children, onClick, disabled, type = "button" }: Props) => {
+export const Button = ({ children, onClick, disabled, type = "button", loading = false }: Props) => {
   return (
     <button
-      className="w-full max-w-[254px] py-[6px] rounded-default bg-primary-700 text-neutral-50 enabled:hover:bg-primary-500 duration-150 ease-in-out enabled:hover:shadow-md hover:shadow-primary-200 enabled:active:bg-primary-800 disabled:bg-secondary-50 disabled:text-secondary-300"
+      className={
+        "w-full items-center py-[6px] rounded-default text-sm sm:text-base bg-primary-700  text-neutral-50 enabled:hover:bg-primary-500 duration-150 ease-in-out enabled:hover:shadow-md hover:shadow-primary-200 enabled:active:bg-primary-800 disabled:bg-secondary-100 disabled:text-secondary-300 flex justify-center"
+      }
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || loading}
       type={type}
     >
-      {children}
+      {loading ? <LoadingIndicator /> : <span className={body.className}>{children}</span>}
     </button>
   )
 }
