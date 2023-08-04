736ae79c38580e222672ca23f2ce1501e22271c4
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-08-04 12:21:32 +0200

    add signout button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+"use client"
 import React from "react"
+import { Button } from "../components/UI"
+import { signOut } from "next-auth/react"
 
 const page = () => {
-  return <div>page</div>
+  return (
+    <div className="w-28">
+      <Button onClick={() => signOut()}>Wyloguj</Button>
+    </div>
+  )
 }
 
 export default page
