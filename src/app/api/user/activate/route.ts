109ef14715bf19b7b71fd4396d99c353bd508208
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-08-07 14:26:02 +0200

    test

--- src/app/api/user/activate/route.ts
+++ src/app/api/user/activate/route.ts
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt"
 export async function POST(req: NextRequest) {
   const body = await req.json()
   const { tokenToActivate, password } = body
-
+  const token = tokenToActivate as string
   if (!tokenToActivate) {
     return new NextResponse("Invalid token", { status: 400 })
   }
@@ -13,7 +13,7 @@ export async function POST(req: NextRequest) {
   const hashedPassword = await bcrypt.hash(password, 10)
   const user = await prisma.user.update({
     where: {
-      tokenToActivate,
+      tokenToActivate: token,
     },
     data: {
       active: true,
