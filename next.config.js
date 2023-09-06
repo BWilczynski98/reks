c533ad08b032eb3b2079e056b0fb51527d3c190b
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-06 12:31:02 +0200

    Set redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ]
+  },
+}
