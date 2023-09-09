aaab5bb544738ff2b9dd6fe3f10ca77520dda9f3
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-09 08:54:31 +0200

    Replaced div element, react fragment element

--- src/app/(layout-with-sidebar)/admin/layout.tsx
+++ src/app/(layout-with-sidebar)/admin/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
 }
 
 const RegisterLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+  return <>{children}</>
 }
 
 export default RegisterLayout
