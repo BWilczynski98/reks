6bbd2313d0f9169b600781b45dcd741992b44264
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-09 08:59:24 +0200

    An endpoint has been created to get a list of all users

---
+++ src/app/api/user/get/route.ts
@@ -0,0 +1,7 @@
+import prisma from "@/app/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  const res = await prisma.user.findMany()
+  return NextResponse.json(res)
+}
