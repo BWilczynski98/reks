9a9a4d3457a5b06b89a9790c88520bdcfad19071
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-09 08:57:12 +0200

    Added a table of users. Changed from client component to server component. Cleaned up imports.

--- src/app/(layout-with-sidebar)/admin/page.tsx
+++ src/app/(layout-with-sidebar)/admin/page.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { Card } from "@/app/components/UI/Card/Card"
+import { TableOfUsers } from "@/app/components/TableOfUsers"
 import { UserCreationForm } from "@/app/components/UserCreationForm"
 import { body } from "@lib/fonts"
 import "react-toastify/dist/ReactToastify.css"
 
 export default function AdminPage() {
   return (
-    <main className={`${body.className} flex flex-col p-2 w-96`}>
-      <Card>
+    <div className={`${body.className} flex flex-col p-2 gap-5 container mx-auto`}>
+      <div className="w-full sm:w-96">
         <UserCreationForm />
-      </Card>
-    </main>
+      </div>
+      <TableOfUsers />
+    </div>
   )
 }
