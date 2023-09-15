b510eb99f458a6d8f41d0e7c590b48c1a48a815c
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-15 16:12:00 +0200

    Handle for navigation to the pet profile

--- src/app/components/UI/Table/TableCell.tsx
+++ src/app/components/UI/Table/TableCell.tsx
@@ -1,13 +1,17 @@
 import { cn } from "@/app/lib/cn"
+import Link from "next/link"
 import React from "react"
 
 type Props = {
   children: React.ReactNode
   align?: "left" | "center" | "right"
   visible?: boolean
+  link?: boolean
+  id?: string
 }
 
-export const TableCell = ({ children, align = "center", visible = true }: Props) => {
+export const TableCell = ({ children, align = "center", visible = true, link, id }: Props) => {
+  const petId = id
   return (
     <td
       className={cn("p-2 max-w-[284px] text-left text-neutral-800", {
@@ -16,7 +20,16 @@ export const TableCell = ({ children, align = "center", visible = true }: Props)
         hidden: !visible,
       })}
     >
-      <p className="line-clamp-1">{children}</p>
+      {link ? (
+        <Link
+          className="line-clamp-1 underline underline-offset-4"
+          href={`/pet/${petId}`}
+        >
+          {children}
+        </Link>
+      ) : (
+        <div className="line-clamp-1">{children}</div>
+      )}
     </td>
   )
 }
