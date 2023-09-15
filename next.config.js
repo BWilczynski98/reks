dee3d274d50d64f7e42962141403749f0bd21512
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-15 16:08:13 +0200

    upadte new domain for demo photo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "modkat.com",
+      },
+    ],
+  },
   async redirects() {
     return [
       {
