75aea934589a955e95139df67caaa2ff041f3677
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-29 14:42:06 +0200

    removal of unnecessary libraries

--- src/app/api/baseUrl.ts
+++
@@ -1,3 +0,0 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production" ? "https://reks-git-refactor-bwilczynski98.vercel.app" : "http://localhost:3000"
-// export const baseUrl = "https://refactor-bwilczynski98-vercel.app"
--- src/lib/fetcher.ts
+++
@@ -1,5 +0,0 @@
-import { baseUrl } from "@/app/api/baseUrl"
-import axios from "axios"
-
-export const fetcher = (url: string, method = "get", data = {}) =>
-  axios({ url: `${baseUrl}/${url}`, method, data }).then((res) => res.data)
