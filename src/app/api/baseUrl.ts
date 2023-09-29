ac13c4d6ccc6028ddc98ebafc38ee51597b91b4b
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-09-29 10:28:20 +0200

    url prod

--- src/app/api/baseUrl.ts
+++ src/app/api/baseUrl.ts
@@ -1,3 +1,5 @@
 export const baseUrl =
-  process.env.NODE_ENV === "production" ? "https://refactor-bwilczynski98-vercel.app" : "http://localhost:3000/api"
+  process.env.NODE_ENV === "production"
+    ? "https://reks-git-refactor-bwilczynski98.vercel.app"
+    : "http://localhost:3000/api"
 // export const baseUrl = "https://refactor-bwilczynski98-vercel.app"
