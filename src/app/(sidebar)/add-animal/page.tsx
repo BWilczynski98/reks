39091ea3709534dd8f82d2e9bd586b600e770352
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-10-15 22:58:49 +0200

    padings

--- src/app/(sidebar)/add-animal/page.tsx
+++ src/app/(sidebar)/add-animal/page.tsx
@@ -2,7 +2,7 @@ import { AnimalForm } from "./components/animal-form"
 
 export default function AddAnimalPage() {
   return (
-    <main className="px-2 pb-[10%] sm:container">
+    <main className="px-2 max-md:pb-[30%] sm:container">
       <AnimalForm />
     </main>
   )
